b4960a606d1d33318b8da6c9f6b56b4343357181
IWTBAGD <103492683+IWTBAGD@users.noreply.github.com>
2024-07-31 11:28:35 +0800

    提交字符串内容

--- Lua学习/记录1.lua
+++ Lua学习/记录1.lua
@@ -19,11 +19,34 @@ print(caozuo2)
 
 --字符串拼接
 caozuo3 = 2
-caozuo4 = "nihao"
+caozuo4 = "nihao=hi"
 print(caozuo3 ..caozuo4)
 
 --格式化输入
 caozuo5 = 18
 caozuo6 = string.format("I'm %d years old",caozuo5)
 print(caozuo6)
+
+--数据类型显式转换
+caozuo7 = 233
+print(tostring(caozuo7))--实际上print会自动隐式转换为string，这个只是做一个演示
+
+-- 字符串公共方法
+caozuo8 = "ABCdef"
+print(string.upper(caozuo8))-- 小写转大写
+print(string.lower(caozuo8))-- 大写转小写
+
+print(string.reverse(caozuo8))-- 字符串翻转
+
+print(string.find(caozuo8,"AB"))-- 查找字符串索引，当搜索到第一个符合要求字符串后会中终止并返回值
+
+print(string.sub(caozuo8,2,4))-- 字符串截取，闭区间，可重载
+
+print(string.rep(caozuo8,3))-- 字符串重复
+
+print(string.gsub(caozuo8,"BC","bc"))-- 字符串修改
+
+print(string.byte(caozuo8,1,3))-- 字符串转换为ASCII
+
+print(string.char(65,66,67))-- ASCII转换为字符串
 --#endregion
